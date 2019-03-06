ed03197d5d9d3db9cf4ec937e4bc62f0a6e6fd98
elmahask <welmahask@gmail.com>
2019-03-06 09:44:45 +0200

    addTripFragment

---
+++ app/src/main/java/iti/jets/tripplanner/fragments/AddNoteFragment.java
@@ -0,0 +1,31 @@
+package iti.jets.tripplanner.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import iti.jets.tripplanner.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddNoteFragment extends Fragment {
+
+
+    public AddNoteFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_note, container, false);
+        return view;
+    }
+
+}
